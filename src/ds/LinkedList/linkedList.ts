469009f232d269b32909cc0379acc1de686faeee
Damian Szyszka <dszys@softserveinc.com>
2021-03-18 01:06:23 +0100

    Fix: fix LinkedList constructor + add getters

--- src/ds/LinkedList/linkedList.ts
+++ src/ds/LinkedList/linkedList.ts
@@ -2,34 +2,49 @@ import LinkedListNode, { ILinkedListNode } from './linkedListNode';
 
 type TLinkedListParameter = any[] | [];
 
-interface ILinkedList {
+export interface ILinkedList {
     append: Function;
     get: Function;
     shift: Function;
     unshift: Function;
     pop: Function;
+    readonly val: any;
+    readonly next: ILinkedListNode | null;
+    readonly length: number;
 };
 
 class LinkedList implements ILinkedList {
-    private head: ILinkedListNode = new LinkedListNode();
+    private listHead: ILinkedListNode = new LinkedListNode();
+    private linkedListLength: number = 0;
 
-    constructor(listStructure: TLinkedListParameter) {
+    constructor(listStructure?: TLinkedListParameter) {
+
         // If listStructure passed, assign its values to linked list
-        if (listStructure.length) {
-            this.head.val = listStructure[0];
-
-            // Won't work -> it must be recursive
-            for (let i = 1; i < listStructure.length; i++) {
+        if (listStructure?.length) {
+            this.listHead.val = listStructure[0];
+            for (let i = 1; i < listStructure.length; i += 2) {
                 let newNode = new LinkedListNode();
                 newNode.val = listStructure[i];
-                newNode.next = listStructure[i + 1] || null;
-
-                this.head.next = newNode;
+                newNode.next = listStructure[i + 1] ? new LinkedListNode(listStructure[i + 1]) : null;
+                this.append(newNode);
             }
         }
     }
 
+    // getters
+    public get val(): ILinkedListNode {
+        return this.listHead.val;
+    } 
+    public get next(): ILinkedListNode {
+        return this.listHead.next;
+    }
+    public get length(): number {
+        return this.linkedListLength;
+    }
+
+    // private methods
     private getLastNode(head: ILinkedListNode): ILinkedListNode {
+        // console.log(head, 'head in getLastNode');
         if (head.next === null) {
             return head;
         }
@@ -38,14 +53,10 @@ class LinkedList implements ILinkedList {
 
     private getNodeAt(index: number): ILinkedListNode {
         // WIP
-        return this.head;
+        return this.listHead;
     }
 
-    private get linkedListLength(): number {
-        // WIP
-
-        return 0;
-    }
+    // public methods
 
     public shift() {
         // WIP
@@ -58,15 +69,15 @@ class LinkedList implements ILinkedList {
     }
 
     public append(newNode: ILinkedListNode): ILinkedListNode {
-        const lastNode: ILinkedListNode = this.getLastNode(this.head);
+        const lastNode: ILinkedListNode = this.getLastNode(this.listHead);
         lastNode.next = newNode;
 
-        return this.head;
+        return this.listHead;
     }
 
     public pop() {
         // WIP
-        const lastNode: ILinkedListNode = this.getLastNode(this.head);
+        const lastNode: ILinkedListNode = this.getLastNode(this.listHead);
 
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import LinkedList from './ds/LinkedList/linkedList';
+import LinkedList, {ILinkedList} from './ds/LinkedList/linkedList';
+
+((): void => {
+    const newLinkedList: ILinkedList = new LinkedList([1,2,3,4,5, {a: 1}, 7]);
+    console.log(newLinkedList.next, 'newLinkedList');
+})();
 
 export {
     LinkedList
