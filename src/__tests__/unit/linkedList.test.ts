bc531ad26158b604aa262f8dce206d92d5ce8643
Damian Szyszka <dszys@softserveinc.com>
2021-03-19 13:29:41 +0100

    Install jest; add jest config file; write down all test cases (without implementation)

---
+++ src/__tests__/unit/linkedList.test.ts
@@ -0,0 +1,29 @@
+import LinkedList, {ILinkedList} from '../../ds/LinkedList/linkedList';
+import {ILinkedListNode} from '../../ds/LinkedList/linkedListNode';
+
+describe('Linked List unit tests', () => {
+
+    describe('Linked List creation tests', () => {
+        it('should create empty linked list', () => {});
+        it('should create linked list from array of objects', () => {});
+    });
+    
+    describe('Getters tests', () => {        
+        it('should return value of current head', () => {});
+        it('should return next value (from current head)', () => {});
+        it('should return length of linked list', () => {});
+        it('should return lastNode', () => {});
+        it('should return head', () => {});
+    });
+
+    describe('Public methods tests', () => {
+        it('should return node at index (.getNodeAt test)', () => {});
+        it('should remove first node (.shift test)', () => {});
+        it('should add element (type any) at the beginning (.unshift test)', () => {});
+        it('should remove last node (.pop test)', () => {});
+        it('should convert and add one element at the end (.add test)', () => {});
+        it('should convert and add many elements at the end (.addMany test)', () => {});
+        it('should insert given element (type any) at the given index (.addMany test)', () => {});
+    });
+    
+});
