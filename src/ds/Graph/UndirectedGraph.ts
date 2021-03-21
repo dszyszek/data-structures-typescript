71ef6969ac6ae47e222c3d89a9393e1c1836309d
Damian Szyszka <dszys@softserveinc.com>
2021-03-21 01:14:03 +0100

    Add UndirectedGraph file

---
+++ src/ds/Graph/UndirectedGraph.ts
@@ -0,0 +1,12 @@
+import Graph from './Graph'
+// import { ILinkedList } from '../LinkedList/LinkedList'
+
+class UndirectedGraph extends Graph {
+    public addEdge = (): void => {}
+
+    public getEdge = (): {} => {
+        return {}
+    }
+}
+
+export default UndirectedGraph
