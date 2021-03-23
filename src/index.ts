2d4424fc907f452f76c1a9538ac6be54075e65f8
Damian Szyszka <dszys@softserveinc.com>
2021-03-23 14:52:49 +0100

    Export all Graphs in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import LinkedList from './ds/LinkedList/LinkedList'
+import UndirectedGraph from './ds/Graph/UndirectedGraph'
+import DirectedGraph from './ds/Graph/DirectedGraph'
+import WeightedDirectedGraph from './ds/Graph/WeightedDirectedGraph'
+import WeightedUndirectedGraph from './ds/Graph/WeightedUndirectedGraph'
 
-export default LinkedList
+export {
+    LinkedList,
+    UndirectedGraph,
+    DirectedGraph,
+    WeightedDirectedGraph,
+    WeightedUndirectedGraph,
+}
