ea5e3059fdcc0b5389e66f0212192b36e0874310
Damian Szyszka <dszys@softserveinc.com>
2021-03-26 17:00:03 +0100

    Implement Stack

--- src/ds/Stack/Stack.ts
+++ src/ds/Stack/Stack.ts
@@ -1,5 +1,41 @@
 import StackAbstract from './StackAbstract'
+import StackNode, { IStackNode, TStackNodeVal } from './StackNode'
+import LinkedList, { ILinkedList } from '../LinkedList/linkedList'
 
-class Stack extends StackAbstract {}
+type TInput = Array<any>
+
+class Stack extends StackAbstract {
+    private stack: ILinkedList = new LinkedList()
+
+    constructor(inputArr?: TInput) {
+        super()
+        if (inputArr?.length) {
+            const convertedToStackNodes = inputArr.map((val) => new StackNode(val))
+            this.stack.addMany(convertedToStackNodes)
+        }
+    }
+
+    public get peak(): IStackNode {
+        return this.stack.head.val
+    }
+
+    public get isEmpty(): Boolean {
+        return !!this.stack.length
+    }
+
+    public push = (val: TStackNodeVal): ILinkedList => {
+        const convertedToStackNode: IStackNode = new StackNode(val)
+        this.stack.insert(0, convertedToStackNode)
+
+        return this.stack
+    }
+
+    public pop = (): IStackNode => {
+        const firstVal: IStackNode = this.stack.head.val
+        this.stack.shift()
+
+        return firstVal
+    }
+}
 
 export default Stack
--- src/ds/Stack/StackAbstract.ts
+++ src/ds/Stack/StackAbstract.ts
@@ -1,5 +1,32 @@
-export interface IStack {}
+/* eslint class-methods-use-this: 0 */
+/* eslint @typescript-eslint/no-unused-vars: 0 */
 
-abstract class StackAbstract implements IStack {}
+import { IStackNode, TStackNodeVal } from './StackNode'
+import { ILinkedList } from '../LinkedList/linkedList'
+
+export interface IStack {
+    readonly peak: IStackNode
+    readonly isEmpty: Boolean
+    push: (val: TStackNodeVal) => IStackNode
+    pop: () => IStackNode
+}
+
+abstract class StackAbstract implements IStack {
+    public get peak(): IStackNode {
+        throw Error('Getter not implemented!')
+    }
+
+    public get isEmpty(): Boolean {
+        throw Error('Getter not implemented!')
+    }
+
+    public push = (val: TStackNodeVal): ILinkedList => {
+        throw Error('Method not implemented!')
+    }
+
+    public pop = (): IStackNode => {
+        throw Error('Method not implemented!')
+    }
+}
 
 export default StackAbstract
