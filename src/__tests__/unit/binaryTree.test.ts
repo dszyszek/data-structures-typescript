609b9462a4f91a0e674c21c34fd2203e51f48aea
Damian Szyszka <dszys@softserveinc.com>
2021-03-31 17:45:14 +0200

    Mock BinaryTree tests

--- src/__tests__/unit/binaryTree.test.ts
+++ src/__tests__/unit/binaryTree.test.ts
@@ -14,15 +14,21 @@ describe('BinaryTree unit tests', () => {
         binaryTree = new BinaryTree(BINARY_TREE_STRUCTURE)
     })
 
-    it('should create binary tree with mocked values', () => {
+    it('should create binary tree with mocked values (constructor + root getter test)', () => {
         const rootNode: IBinaryTreeNode = binaryTree.root
 
         expect(rootNode.val).toEqual(12)
-        expect(rootNode?.right?.val).toEqual(34)
-        expect(rootNode?.left?.val).toEqual(1)
-        expect(rootNode?.right?.left?.val).toEqual(22)
-        expect(rootNode?.right?.right).toEqual(null)
-        expect(rootNode?.left?.right?.val).toEqual(5)
-        expect(rootNode?.left?.left).toEqual(null)
+        expect(rootNode.right?.val).toEqual(34)
+        expect(rootNode.left?.val).toEqual(1)
+        expect(rootNode.right?.left?.val).toEqual(22)
+        expect(rootNode.right?.right).toEqual(null)
+        expect(rootNode.left?.right?.val).toEqual(5)
+        expect(rootNode.left?.left).toEqual(null)
     })
+
+    it('should add new node to tree (.add() test)', () => {})
+    it('should add new tree nodes (.addMany() test)', () => {})
+    it('should return true after "contains" check (.contains() test)', () => {})
+    it('should return false after "contains" check (.contains() test)', () => {})
+    it('should delete tree node (.delete() test)', () => {})
 })
