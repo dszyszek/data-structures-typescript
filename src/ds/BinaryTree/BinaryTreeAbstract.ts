60280c45b0deb2711bb8174e72adede88dbebe72
Damian Szyszka <dszys@softserveinc.com>
2021-03-31 15:14:46 +0200

    Implement BinaryTreeNode + BinaryTreeAbstract

--- src/ds/BinaryTree/BinaryTreeAbstract.ts
+++ src/ds/BinaryTree/BinaryTreeAbstract.ts
@@ -3,11 +3,21 @@
 import { TBinaryTreeVal, IBinaryTreeNode } from './BinaryTreeNode'
 
 interface IBinaryTree {
-    addNode: (val: TBinaryTreeVal) => IBinaryTreeNode
+    add: (val: TBinaryTreeVal) => IBinaryTreeNode
+    contains: (val: TBinaryTreeVal) => boolean
+    delete: (val: TBinaryTreeVal) => IBinaryTreeNode
 }
 
 abstract class BinaryTree implements IBinaryTree {
-    public addNode = (val: TBinaryTreeVal): IBinaryTreeNode => {
+    public add = (val: TBinaryTreeVal): IBinaryTreeNode => {
+        throw Error('Method not implemented!')
+    }
+
+    public contains = (val: TBinaryTreeVal): boolean => {
+        throw Error('Method not implemented!')
+    }
+
+    public delete = (val: TBinaryTreeVal): IBinaryTreeNode => {
         throw Error('Method not implemented!')
     }
 }
--- src/ds/BinaryTree/BinaryTreeNode.ts
+++ src/ds/BinaryTree/BinaryTreeNode.ts
@@ -1,6 +1,22 @@
 export type TBinaryTreeVal = any
-export interface IBinaryTreeNode {}
+export interface IBinaryTreeNode {
+    val: TBinaryTreeVal
+    left: IBinaryTreeNode | null
+    right: IBinaryTreeNode | null
+}
 
-class BinaryTreeNode implements IBinaryTreeNode {}
+class BinaryTreeNode implements IBinaryTreeNode {
+    public val: TBinaryTreeVal
+
+    public left: IBinaryTreeNode | null = null
+
+    public right: IBinaryTreeNode | null = null
+
+    constructor(val: TBinaryTreeVal = null, left?: TBinaryTreeVal, right?: TBinaryTreeVal) {
+        this.val = val
+        this.left = new BinaryTreeNode(left)
+        this.right = new BinaryTreeNode(right)
+    }
+}
 
 export default BinaryTreeNode
