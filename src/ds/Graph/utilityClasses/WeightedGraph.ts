77d22d6b6c0752ec6ad5da5b3ffa867bcf727fbf
Damian Szyszka <dszys@softserveinc.com>
2021-04-03 23:19:09 +0200

    Fix WeightedGraph.display method (threw errors)

--- src/ds/Graph/utilityClasses/WeightedGraph.ts
+++ src/ds/Graph/utilityClasses/WeightedGraph.ts
@@ -7,12 +7,12 @@ import { TVertexValue } from './Vertex'
 class WeightedGraph extends Graph {
     public display = (): void => {
         this.adjList.forEach((list, vertex) => {
-            let currentEgdeList: ILinkedListNode = list.head
+            let currentEgdeList: ILinkedListNode = list.head.next
 
-            while (currentEgdeList.next) {
+            while (currentEgdeList) {
                 const { source, destination, weight } = currentEgdeList.val
                 const adjacentVertex: TVertexValue =
-                    vertex === source.val ? source.val : destination.val
+                    vertex === source.val ? destination.val : source.val
                 console.log(`Vertex ${vertex} is adjacent to: ${adjacentVertex}, weight: ${weight}`)
                 currentEgdeList = currentEgdeList.next
             }
